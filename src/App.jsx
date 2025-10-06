cab6192a4aaa8fffc1edc6ea249f4d1b7c3e84a9
AbhishekWMG <ak89471@gmail.com>
2025-10-06 17:08:10 +0530

    Game Logic 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 import gameIcon from "../public/game-logo.png";
+import { useState } from "react";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        {
+          square: {
+            row: rowIndex,
+            col: colIndex,
+          },
+          player: currentPlayer,
+        },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
+  };
+
   return (
     <main>
       <header>
@@ -9,12 +39,21 @@ function App() {
         <h1>Tic-Tac-Toe</h1>
       </header>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        GAME BOARD
+        <GameBoard turns={gameTurns} onSelectSquare={handleSelectSquare} />
       </div>
+      <Log turns={gameTurns} />
     </main>
   );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { isValidElement, useState } from "react";
 
-const Player = ({ initialName, symbol }) => {
+const Player = ({ initialName, symbol, isActive }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initialName);
 
@@ -23,7 +23,7 @@ const Player = ({ initialName, symbol }) => {
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
